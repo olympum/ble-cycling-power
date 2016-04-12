37dafea9064032105fc87040b5f55c67d6a505df
hcrohland <christoph@Hobbes.fritz.box>
2016-04-12 22:28:44 +0200

    Add speed data
    mimic a 2095 mm circumference wheel

--- cycling-power-measurement-characteristic.js
+++ cycling-power-measurement-characteristic.js
@@ -44,12 +44,16 @@ CyclingPowerMeasurementCharacteristic.prototype.onUnsubscribe = function() {
   this._updateValueCallback = null;
 };
 
+var last_speed = Date.now();
+var last_rev = 0;
+var wheel_circ = 2095;
+
 CyclingPowerMeasurementCharacteristic.prototype.notify = function(event) {
   if (!('watts' in event)) {
     // ignore events with no power data
     return;
   }
-  var buffer = new Buffer(8);
+  var buffer = new Buffer(14);
   buffer.fill(0); //initialize buffer
   
   // flags
@@ -62,25 +66,45 @@ CyclingPowerMeasurementCharacteristic.prototype.notify = function(event) {
   // 01000000 - 64  - 0x040 - Extreme Force Magnitudes Present
   // 10000000 - 128 - 0x080 - Extreme Torque Magnitudes Present
   var flags = 0;
+  var pos = 4;
+  var now = Date.now();
   var watts = event.watts;
 
   debug("power: " + watts);
   buffer.writeInt16LE(watts, 2);
   
+  if ('speed_cm_s' in event) {
+    var delta = (now - last_speed);
+    var speed = event.speed_cm_s * 10; // mm/s
+    var rev = Math.floor (speed * delta / wheel_circ / 1000);
+    debug("\n\rspeed: " + speed + " rev: " + rev);
+    debug ('now/last: ' + now +  ' / ' + last_speed);
+    if (rev != 0) {
+      last_rev += rev;
+      last_speed += Math.floor ((wheel_circ * rev) * 1000 / speed);
+      debug("new_speed/delta: " + last_speed + " / " + delta);
+      buffer.writeUInt32LE(last_rev, pos);
+      buffer.writeUInt16LE((last_speed * 2048 / 1000)% 65536, pos+4);
+      pos += 6;
+      flags |= 0x010;
+    }
+  }
+
   if ('rev_count' in event) {
     debug("rev_count: " + event.rev_count);
-    buffer.writeUInt16LE(event.rev_count, 4);
-
-    var now = Date.now();
+    buffer.writeUInt16LE(event.rev_count, pos);
+    
     var now_1024 = Math.floor(now*1024/1000);
     var event_time = now_1024 % 65536; // rolls over every 64 seconds
     debug("event time: " + event_time);
-    buffer.writeUInt16LE(event_time, 6);
+    buffer.writeUInt16LE(event_time, pos+2);
+    pos += 4;
     flags |= 0x020;
   }
 
   buffer.writeUInt16LE(flags, 0);
 
+  debug ("Send: " + buffer.toString('hex'));
   if (this._updateValueCallback) {
     this._updateValueCallback(buffer);
   }
