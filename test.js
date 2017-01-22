5498a36d230132e5cec8227b3b6d4bcb892e7f7c
Bruno Fernandez-Ruiz <b@getnexar.com>
2017-01-22 12:05:32 +0000

    Example usage

---
+++ test.js
@@ -0,0 +1,14 @@
+var bcp = new require('ble-cycling-power');
+
+var peripheral = new bcp.BluetoothPeripheral('TestDevice');
+
+console.log("[Init] Faking test data");
+
+var stroke_count = 0;
+var test = function() {
+  var watts = Math.floor(Math.random() * 10 + 120);
+  stroke_count = stroke_count + 1;
+  peripheral.notify({'watts': watts, 'rev_count': stroke_count});
+  setTimeout(test, 249);
+};
+test();
